a16ebb34c51b7d5868b8f41f8f28fdaa365e4a01
brian_ogola <brianochiengogola78@gmail.com>
2023-10-13 11:20:05 +0300

    About page

--- src/components/HomeMaster/NavbarComponent.vue
+++ src/components/HomeMaster/NavbarComponent.vue
@@ -9,8 +9,8 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <router-link to="/" class="nav-item nav-link">Home</router-link>
+          <router-link to="/about" class="nav-item nav-link">About</router-link>
           <li><a class="nav-link scrollto" href="#services">Services</a></li>
           <li><a class="nav-link   scrollto" href="#portfolio">Portfolio</a></li>
           <li><a class="nav-link scrollto" href="#team">Team</a></li>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,26 +1,6 @@
 <template>
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
-
-<div class="container">
-  <div class="row">
-    <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-      <h1>Savycode Solutions Limited</h1>
-      <h2>We are team of talented designers making websites that suits your Business</h2>
-      <div class="d-flex justify-content-center justify-content-lg-start">
-        <a href="#about" class="btn-get-started scrollto" style="color: green;">Get In Touch</a>
-      </div>
-    </div>
-    <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-      <img src="../assets/img/hero-img.png" class="img-fluid animated" alt="">
-    </div>
-  </div>
-</div>
-
-</section><!-- End Hero -->
-
- <!-- ======= About Us Section ======= -->
- <section id="about" class="about">
+  <!-- ======= About Us Section ======= -->
+  <section id="about" class="about">
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
