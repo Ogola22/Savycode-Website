140702a7876b78190d6eabef5853316b8618c220
brian_ogola <brianochiengogola78@gmail.com>
2023-10-16 15:56:25 +0300

    Edited contact page and home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -246,8 +246,7 @@
                             </div>
                             <div class="ms-3">
                                 <h4 class="text-primary">Address</h4>
-                                <a href="https://goo.gl/maps/Zd4BCynmTb98ivUJ6" target="_blank" class="h5">23 rank Str,
-                                    NY</a>
+                                <a href="https://goo.gl/maps/Zd4BCynmTb98ivUJ6" target="_blank" class="h5">00100, Nairobi</a>
                             </div>
                         </div>
                     </div>
@@ -259,7 +258,7 @@
                             </div>
                             <div class="ms-3">
                                 <h4 class="text-primary">Call Us</h4>
-                                <a class="h5" href="tel:+0123456789" target="_blank">+012 3456 7890</a>
+                                <a class="h5" href="tel:+0123456789" target="_blank">+254 700458981</a>
                             </div>
                         </div>
                     </div>
@@ -271,7 +270,7 @@
                             </div>
                             <div class="ms-3">
                                 <h4 class="text-primary">Email Us</h4>
-                                <a class="h5" href="mailto:info@example.com" target="_blank">info@example.com</a>
+                                <a class="h5" href="mailto:info@example.com" target="_blank">info@savycode.co.ke</a>
                             </div>
                         </div>
                     </div>
@@ -280,7 +279,7 @@
                     <div class="col-lg-6 wow fadeIn" data-wow-delay=".3s">
                         <div class="p-5 h-100 rounded contact-map">
                             <iframe class="rounded w-100 h-100"
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.4710403339755!2d-73.82241512404069!3d40.685622471397615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c26749046ee14f%3A0xea672968476d962c!2s123rd%20St%2C%20Queens%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1686493221834!5m2!1sen!2sbd"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d255282.32390058352!2d36.68257878578605!3d-1.3032035598814729!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1172d84d49a7%3A0xf7cf0254b297924c!2sNairobi!5e0!3m2!1sen!2ske!4v1697460741054!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
                                 style="border:0;" allowfullscreen="" loading="lazy"
                                 referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
